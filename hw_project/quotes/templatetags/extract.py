fed1618395e733fff5d9108c356960a7ab455c02
0101saber <0101saber@gmail.com>
2024-11-26 15:10:37 +0200

    first commit

--- hw_project/quotes/templatetags/extract.py
+++ hw_project/quotes/templatetags/extract.py
@@ -1,14 +1,16 @@
 from django import template
 from bson.objectid import ObjectId
 
+from ..models import Author, Quote
 from ..utils import get_mongo_db
 
 register = template.Library()
 
 
 def get_author(id_):
-    db = get_mongo_db()
-    author = db.authors.find_one({'_id': ObjectId(id_)})
+    author = Author.objects.get(id=id_)
+    if not author:
+        return 'No such author'
     return author['fullname']
 
 
